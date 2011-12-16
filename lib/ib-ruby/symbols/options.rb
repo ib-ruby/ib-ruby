83e537ec13abe2234689a798e91192cff659590a
arvicco <arvitallian@gmail.com>
2011-12-16 11:07:40 -0800

    Option symbols added

--- lib/ib-ruby/symbols/options.rb
+++ lib/ib-ruby/symbols/options.rb
@@ -10,18 +10,26 @@ module IB
     Options =
         {:wfc20 => Models::Contract.new(:symbol => "WFC",
                                         :exchange => "SMART",
-                                        :expiry => "201112",
+                                        :expiry => "201207",
                                         :right => "CALL",
                                         :strike => 20.0,
                                         :sec_type => SECURITY_TYPES[:option],
-                                        :description => "Wells Fargo 20 Call 2011-12"),
+                                        :description => "Wells Fargo 20 Call 2012-07"),
          :z50 => Models::Contract.new(:symbol => "Z",
                                       :exchange => "LIFFE",
-                                      :expiry => "201112",
+                                      :expiry => "201206",
                                       :right => "CALL",
                                       :strike => 50.0,
                                       :sec_type => SECURITY_TYPES[:option],
-                                      :description => " FTSE-100 index 50 Call 2011-12"),
+                                      :description => " FTSE-100 index 50 Call 2012-06"),
+         :spy => Models::Contract.new(:symbol => 'SPY',
+                                      :exchange => "SMART",
+                                      :expiry => "20120615",
+                                      :right => "P",
+                                      :currency => "USD",
+                                      :strike => 75.0,
+                                      :sec_type => SECURITY_TYPES[:option],
+                                      :description => "SPY 75.0 Put 2012-06-16")
 
         }
   end
