afab30f58a2476d1903f55688944d97a758fa678
arvicco <arvitallian@gmail.com>
2012-02-07 12:39:54 +0300

    Message spec codestyled a bit

--- spec/ib-ruby/messages/incoming_spec.rb
+++ spec/ib-ruby/messages/incoming_spec.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), %w[../.. spec_helper])
+require 'spec_helper'
 require 'thread'
 
 describe IB::Messages::Incoming do
@@ -17,37 +17,36 @@ describe IB::Messages::Incoming do
             it { should_not be_error }
           end
         end
-        
+
         @subscriber_tick_price = @ib.subscribe(:TickPrice) do |msg|
           @count << msg
-          
+
           context "message on tick price" do
             subject { msg }
 
             it { should_not be_nil }
-            its (:type) {should_not be_nil}
-            its (:data) {should be_a Hash}
-            its (:data) {should have_key(:id)}
-            
+            its(:type) {should_not be_nil}
+            its(:data) {should be_a Hash}
+            its(:data) {should have_key(:id)}
+
           end
-        
         end
-        
+
         @subscriber_tick_size = @ib.subscribe(:TickSize) do |msg|
           context "message on tick size" do
             subject { msg }
 
             it { should_not be_nil }
-            its (:type) {should_not be_nil}
-            its (:data) {should be_a Hash}
-            its (:data) {should have_key(:id)}
-            
+            its(:type) {should_not be_nil}
+            its(:data) {should be_a Hash}
+            its(:data) {should have_key(:id)}
+
           end
         end
-        
+
         #TODO consider a follow the sun market lookup for windening the types tested
         @ib.send_message :RequestMarketData, :id => 456, :contract => IB::Symbols::Forex[:eurusd]
-        
+
         #Wait for 3 tick prices events or 30 seconds
         breaker = 0
         until @count.size > 3 || breaker > 30 do
@@ -58,9 +57,9 @@ describe IB::Messages::Incoming do
         @ib.close if @ib
 
     end # Subscription Market Data and receive Tick Price and Tick Size msg's
-  
+
     context "Subscribe to Market Data and error on receive Tick Price and Tick Size msg's" do
-      
+
         RSpec::Mocks::setup(self) # did not figure out how to do this just for logger
 
         @count =  Queue.new
@@ -69,22 +68,22 @@ describe IB::Messages::Incoming do
           @count << msg
           puts msg.to_human
           context "message on Alert" do
-            
+
             subject { msg }
             it { should be_warning }
             it { should_not be_error }
            end
-        end      
-                
+        end
+
         log.should_receive(:warn).with("No subscribers for message #{IB::Messages::Incoming::OpenOrderEnd}!").at_least(:once)
         log.should_receive(:warn).with("No subscribers for message #{IB::Messages::Incoming::TickPrice}!").at_least(:once)
         log.should_receive(:warn).with("No subscribers for message #{IB::Messages::Incoming::TickSize}!").at_least(:once)
         #puts "\n\object.methods : "+ log.methods.sort.join("\n").to_s+"\n\n"
-         
+
         @ib.send_message :RequestMarketData, :id => 456, :contract => IB::Symbols::Forex[:eurusd]
-      
+
         #Wait for 3 tick prices events or 30 seconds
-        #TODO Think about a stub implimentation of log.warn to emmit counts.
+        #TODO Think about a stub implementation of log.warn to emit counts.
         breaker = 0
         until @count.size > 3 || breaker > 30 do
           sleep 1
@@ -94,8 +93,5 @@ describe IB::Messages::Incoming do
         @ib.close if @ib
 
     end # Subscription Market Data and receive Tick Price and Tick Size msg's
-        
   end # connected
-
-
 end # describe IB::Messages::Incomming
