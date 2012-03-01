5662b1d16c53ac9fb5311eab7aff4d1f87748740
arvicco <arvitallian@gmail.com>
2012-03-01 17:45:01 +0400

    Contract info spec for stocks (AAPL)

--- spec/integration/contract_info_spec.rb
+++ spec/integration/contract_info_spec.rb
@@ -10,19 +10,72 @@ describe "Request Contract Info", :connected => true, :integration => true do
 
   after(:all) { close_connection }
 
+  context "Request Stock data" do
+
+    before(:all) do
+      @contract = IB::Models::Contract.new :symbol => 'AAPL',
+                                           :sec_type => IB::SECURITY_TYPES[:stock]
+      @ib.send_message :RequestContractData,
+                       :id => 111,
+                       :contract => @contract
+
+      wait_for(3) { received? :ContractDataEnd }
+    end
+
+    after(:all) { clean_connection } # Clear logs and message collector
+
+    it { @received[:ContractData].should have_exactly(2).contract_data }
+    it { @received[:ContractDataEnd].should have_exactly(1).contract_data_end }
+
+    it 'receives Contract Data for requested contract' do
+      msg = @received[:ContractData].first
+      msg.request_id.should == 111
+      msg.contract.should == @contract
+    end
+
+    it 'receives Contract Data with extended fields' do
+      # Returns 2 contracts, one for NASDAQ and one for IBIS - internal crossing?
+
+      @received[:ContractData].each do |msg|
+        contract = msg.contract
+        contract.symbol.should == 'AAPL'
+
+        contract.local_symbol.should =~ /AAPL|APC/
+        contract.market_name.should =~ /NMS|USSTARS/
+        contract.trading_class.should =~ /NMS|USSTARS/
+        contract.long_name.should == 'APPLE INC'
+        contract.industry.should == 'Technology'
+        contract.category.should == 'Computers'
+        contract.subcategory.should == 'Computers'
+        contract.exchange.should == 'SMART'
+        contract.con_id.should be_an Integer
+        contract.trading_hours.should =~ /\d{8}:\d{4}-\d{4}/
+        contract.liquid_hours.should =~ /\d{8}:\d{4}-\d{4}/
+        contract.valid_exchanges.should =~ /ISLAND|IBIS/
+        contract.order_types.should be_a String
+        contract.price_magnifier.should == 1
+        contract.min_tick.should be <= 0.01
+
+        contract.expiry.should be_nil
+      end
+    end
+  end # Stock
+
   context "Request Option data" do
 
     before(:all) do
       @contract = IB::Models::Contract::Option.new :symbol => "AAPL",
-                                               :expiry => "201301",
-                                               :right => "CALL",
-                                               :strike => 500
+                                                   :expiry => "201301",
+                                                   :right => "CALL",
+                                                   :strike => 500
       @ib.send_message :RequestContractData,
                        :id => 123,
                        :contract => @contract
 
-      wait_for(3) { received? :ContractData }
+      wait_for(3) { received? :ContractDataEnd }
     end
+
+    after(:all) { clean_connection } # Clear logs and message collector
 
     subject { @received[:ContractData].first }
 
@@ -47,10 +100,10 @@ describe "Request Contract Info", :connected => true, :integration => true do
       contract.subcategory.should == 'Computers'
       contract.expiry.should == '20130118'
       contract.exchange.should == 'SMART'
-      contract.con_id.should == 82635631
+      contract.con_id.should be_an Integer
       contract.trading_hours.should =~ /\d{8}:\d{4}-\d{4}/
       contract.liquid_hours.should =~ /\d{8}:\d{4}-\d{4}/
-      contract.valid_exchanges.should =~ /CBOE|SMART/
+      contract.valid_exchanges.should =~ /CBOE/
       contract.order_types.should be_a String
       contract.price_magnifier.should == 1
       contract.min_tick.should == 0.01
