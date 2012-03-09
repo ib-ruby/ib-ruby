1861d43b8bb9ed084895f4a3056db346578deb36
arvicco <arvitallian@gmail.com>
2012-03-09 20:14:57 +0400

    Codestyling

--- lib/ib-ruby/models/execution.rb
+++ lib/ib-ruby/models/execution.rb
@@ -5,27 +5,26 @@ module IB
     # This is IB Order execution report.
     # Instantiate with a Hash of attributes, to be auto-set via initialize in Model.
     class Execution < Model
-      attr_accessor :order_id, #      int: order id. TWS orders have a fixed order id of 0.
-                    :client_id, #     int: id of the client that placed the order.
-                    #                      TWS orders have a fixed client id of 0.
-                    :exec_id, #       String: Unique order execution id.
-                    :time, #          String: The order execution time.
-                    :account_name, #String: The customer account number.
-                    :exchange, #      String: Exchange that executed the order.
-                    :side, #          String: Was the transaction a buy or a sale: BOT|SLD
-                    :shares, #        int: The number of shares filled.
-                    :price, #         double: The order execution price.
-                    :perm_id, #       int: TWS id used to identify orders, remains
+      attr_accessor :order_id, #     int: order id. TWS orders have a fixed order id of 0.
+                    :client_id, #    int: id of the client that placed the order.
+                    #                     TWS orders have a fixed client id of 0.
+                    :perm_id, #      int: TWS id used to identify orders, remains
+                    :exec_id, #      String: Unique order execution id.
                     #                      the same over TWS sessions.
-                    :liquidation, #   int: Identifies the position as one to be liquidated
-                    #                      last should the need arise.
+                    :time, #         String: The order execution time.
+                    :exchange, #     String: Exchange that executed the order.
+                    :side, #         String: Was the transaction a buy or a sale: BOT|SLD
+                    :account_name, # String: The customer account number.
+                    :price, #        double: The order execution price.
+                    :average_price, # double: Average price. Used in regular trades, combo
+                    #                         trades and legs of the combo.
+                    :shares, #       int: The number of shares filled.
                     :cumulative_quantity, # int: Cumulative quantity. Used in regular
-                    #                           trades, combo trades and legs of the combo
-                    :average_price #  double: Average price. Used in regular trades, combo
-                                   #          trades and legs of the combo.
-      # Legacy
-      alias account_number account_name
-      alias account_number= account_name=
+                    #                            trades, combo trades and legs of the combo
+                    :liquidation #  int: This position is liquidated last should the need arise.
+
+      alias account_number account_name # Legacy
+      alias account_number= account_name= # Legacy
 
       def side= value
         @side = case value
@@ -50,9 +49,9 @@ module IB
       end
 
       def to_s
-        "<Execution #{@time}: #{@side} #{@shares} @ #{@price} on #{@exchange}, " +
-            "cumulative: #{@cumulative_quantity} @ #{@average_price}, " +
-            "ids: #{@order_id} order, #{@perm_id} perm, #{@exec_id} exec>"
+        "<Execution #{time}: #{side} #{shares} @ #{price} on #{exchange}, " +
+            "cumulative: #{cumulative_quantity} @ #{average_price}, " +
+            "ids: #{order_id} order, #{perm_id} perm, #{exec_id} exec>"
       end
     end # Execution
   end # module Models
