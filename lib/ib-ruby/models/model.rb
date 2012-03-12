6ecb8bbac447c186b9efe54be8a7c25be6c89cba
arvicco <arvitallian@gmail.com>
2012-03-12 16:08:45 +0400

    Model.prop macro added

--- lib/ib-ruby/models/model.rb
+++ lib/ib-ruby/models/model.rb
@@ -1,18 +1,24 @@
+require 'ib-ruby/models/model_properties'
+
 module IB
   module Models
 
     # Base IB data Model class, in future it will be developed into ActiveModel
     class Model
+      extend ModelProperties
+
       attr_reader :created_at
+
+      DEFAULT_PROPS = {}
 
       # If a opts hash is given, keys are taken as attribute names, values as data.
       # The model instance fields are then set automatically from the opts Hash.
-      #
       def initialize(opts={})
         raise ArgumentError.new("Argument must be a Hash") unless opts.is_a?(Hash)
         @created_at = Time.now
 
-        opts.keys.each { |key| self.send("#{key}=", opts[key]) }
+        props = self.class::DEFAULT_PROPS.merge(opts)
+        props.keys.each { |key| self.send("#{key}=", props[key]) }
       end
 
       # ActiveModel-style attribute accessors
