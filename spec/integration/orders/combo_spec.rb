e199aa3d6978604ed5e6bcba121c4d7fb16d5633
arvicco <arvitallian@gmail.com>
2012-03-23 11:44:04 +0400

    Attached Orders specified

--- spec/integration/orders/combo_spec.rb
+++ spec/integration/orders/combo_spec.rb
@@ -28,7 +28,7 @@ def butterfly symbol, expiry, right, *strikes
                                 :legs => legs
 end
 
-describe "Combo Order", :connected => true, :integration => true do
+describe "Combo Order", :connected => true, :integration => true, :slow => true do
 
   before(:all) { verify_account }
 
@@ -38,14 +38,60 @@ describe "Combo Order", :connected => true, :integration => true do
       @ib.wait_for :NextValidId
       @ib.clear_received # to avoid conflict with pre-existing Orders
 
-      @combo = butterfly 'GOOG', '201301', 'CALL', 500, 510, 520
+      @contract = butterfly 'GOOG', '201301', 'CALL', 500, 510, 520
 
-      place_order @combo, :limit_price => 0.01 #, :what_if => true
-      @ib.wait_for [:OpenOrder, 3], [:OrderStatus, 2], 5
+      place_order @contract, :limit_price => 0.01 #, :what_if => true
+      @ib.wait_for :OpenOrder, :OrderStatus, 5
     end
 
     after(:all) { close_connection }
 
     it_behaves_like 'Placed Order'
   end # Limit
+
+  context "Limit with attached takeprofit" do
+    before(:all) do
+      @ib = IB::Connection.new OPTS[:connection].merge(:logger => mock_logger)
+      @ib.wait_for :NextValidId
+      @ib.clear_received # to avoid conflict with pre-existing Orders
+
+      @contract = butterfly 'GOOG', '201301', 'CALL', 500, 510, 520
+
+      place_order @contract, :limit_price => 0.01, :transmit => false
+      @ib.wait_for :OpenOrder, :OrderStatus, 2
+    end
+
+    after(:all) { close_connection }
+
+    it 'does not transmit original Order just yet' do
+      @ib.received[:OpenOrder].should have_exactly(0).order_message
+      @ib.received[:OrderStatus].should have_exactly(0).status_message
+    end
+
+    context 'Attaching takeprofit' do
+      before(:all) do
+        @attached_order = IB::Models::Order.new :total_quantity => 100,
+                                                :limit_price => 0.5,
+                                                :action => 'SELL',
+                                                :order_type => 'LMT',
+                                                :parent_id => @order_id_placed
+
+        @order_id_attached = @ib.place_order @attached_order, @contract
+        @order_id_after = @ib.next_order_id
+        @ib.wait_for :OpenOrder, :OrderStatus, 5
+      end
+
+      it_behaves_like 'Placed Order'
+    end
+
+    context 'When original Order cancels' do
+      it 'attached takeprofit is cancelled implicitely' do
+        @ib.send_message :RequestOpenOrders
+        @ib.wait_for :OpenOrderEnd
+        @ib.received[:OpenOrder].should have_exactly(0).order_message
+        @ib.received[:OrderStatus].should have_exactly(0).status_message
+      end
+    end
+  end # Attached
 end # Combo Orders
+
