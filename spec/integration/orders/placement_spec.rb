53471dc88fdb2de85b469382462169542e04133a
arvicco <arvitallian@gmail.com>
2012-03-30 16:07:41 +0400

    Attached takeprofit for stock orders

--- spec/integration/orders/placement_spec.rb
+++ spec/integration/orders/placement_spec.rb
@@ -60,7 +60,7 @@ describe 'Orders', :connected => true, :integration => true do
     it { @ib.received[:OpenOrder].should have_at_least(1).open_order_message }
     it { @ib.received[:OrderStatus].should have_exactly(0).status_messages }
 
-    it 'returns as what-if Order with margin and commission info' do
+    it 'responds with margin and commission info' do
       order_should_be /PreSubmitted/
       order = @ib.received[:OpenOrder].first.order
       order.what_if.should == true
@@ -74,7 +74,7 @@ describe 'Orders', :connected => true, :integration => true do
       order.commission.should be > 1
     end
 
-    it 'is not actually opened though' do
+    it 'is not actually being placed though' do
       @ib.clear_received
       @ib.send_message :RequestOpenOrders
       @ib.wait_for :OpenOrderEnd
@@ -82,7 +82,7 @@ describe 'Orders', :connected => true, :integration => true do
     end
   end
 
-  context 'Off-market stock order' do
+  context 'Off-market limit' do
     before(:all) do
       @ib = IB::Connection.new OPTS[:connection].merge(:logger => mock_logger)
       @ib.wait_for :NextValidId
@@ -120,5 +120,52 @@ describe 'Orders', :connected => true, :integration => true do
         alert.message.should =~ /Can't find order with id =/
       end
     end # Cancelling
-  end # Off-market order
+  end # Off-market limit
+
+  context "Limit with attached takeprofit" do
+    before(:all) do
+      @ib = IB::Connection.new OPTS[:connection].merge(:logger => mock_logger)
+      @ib.wait_for :NextValidId
+      @ib.clear_received # to avoid conflict with pre-existing Orders
+
+      place_order IB::Symbols::Stocks[:wfc],
+                  :limit_price => 9.13,
+                  :transmit => false
+
+      @ib.wait_for :OpenOrder, :OrderStatus, 2
+    end
+
+    after(:all) { close_connection }
+
+    it 'does not transmit original Order just yet' do
+      @ib.received[:OpenOrder].should have_exactly(0).order_message
+      @ib.received[:OrderStatus].should have_exactly(0).status_message
+    end
+
+    context 'Attaching takeprofit' do
+      before(:all) do
+        @attached_order = IB::Models::Order.new :total_quantity => 100,
+                                                :limit_price => 15.0,
+                                                :action => 'SELL',
+                                                :order_type => 'LMT',
+                                                :parent_id => @order_id_placed
+
+        @order_id_attached = @ib.place_order @attached_order, IB::Symbols::Stocks[:wfc]
+        @order_id_after = @ib.next_order_id
+        @ib.wait_for [:OpenOrder, 2], [:OrderStatus, 2], 5
+      end
+
+      it_behaves_like 'Placed Order'
+    end
+
+    context 'When original Order cancels' do
+      it 'attached takeprofit is cancelled implicitly' do
+        @ib.send_message :RequestOpenOrders
+        @ib.wait_for :OpenOrderEnd
+        @ib.received[:OpenOrder].should have_exactly(0).order_message
+        @ib.received[:OrderStatus].should have_exactly(0).status_message
+      end
+    end
+  end # Attached
+
 end # Orders
