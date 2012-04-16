0773bf1cc3e9a29afd7995857fdcb0bac905ec15
arvicco <arvitallian@gmail.com>
2012-04-16 18:02:18 +0400

    AbstractMessages extracted into separate files

--- lib/ib-ruby/messages/incoming.rb
+++ lib/ib-ruby/messages/incoming.rb
@@ -1,4 +1,4 @@
-require 'ib-ruby/messages/abstract_message'
+require 'ib-ruby/messages/incoming/abstract_message'
 
 # EClientSocket.java uses sendMax() rather than send() for a number of these.
 # It sends an EOL rather than a number if the value == Integer.MAX_VALUE (or Double.MAX_VALUE).
@@ -15,100 +15,7 @@ module IB
     module Incoming
       extend Messages # def_message macros
 
-      # Container for specific message classes, keyed by their message_ids
-      Classes = {}
-
-      class AbstractMessage < IB::Messages::AbstractMessage
-
-        def version # Per message, received messages may have the different versions
-          @data[:version]
-        end
-
-        def check_version actual, expected
-          unless actual == expected || expected.is_a?(Array) && expected.include?(actual)
-            error "Unsupported version #{actual} received, expected #{expected}"
-          end
-        end
-
-        # Create incoming message from a given source (IB server or data Hash)
-        def initialize source
-          @created_at = Time.now
-          if source[:socket] # Source is a server
-            @server = source
-            @data = Hash.new
-            begin
-              self.load
-            rescue => e
-              error "Reading #{self.class}: #{e.class}: #{e.message}", :load, e.backtrace
-            ensure
-              @server = nil
-            end
-          else # Source is a @data Hash
-            @data = source
-          end
-        end
-
-        def socket
-          @server[:socket]
-        end
-
-        # Every message loads received message version first
-        # Override the load method in your subclass to do actual reading into @data.
-        def load
-          @data[:version] = socket.read_int
-
-          check_version @data[:version], self.class.version
-
-          load_map *self.class.data_map
-        end
-
-        # Load @data from the socket according to the given data map.
-        #
-        # map is a series of Arrays in the format of
-        #   [ :name, :type ], [  :group, :name, :type]
-        # type identifiers must have a corresponding read_type method on socket (read_int, etc.).
-        # group is used to lump together aggregates, such as Contract or Order fields
-        def load_map(*map)
-          map.each do |instruction|
-            # We determine the function of the first element
-            head = instruction.first
-            case head
-              when Integer # >= Version condition: [ min_version, [map]]
-                load_map *instruction.drop(1) if version >= head
-
-              when Proc # Callable condition: [ condition, [map]]
-                load_map *instruction.drop(1) if head.call
-
-              when true # Pre-condition already succeeded!
-                load_map *instruction.drop(1)
-
-              when nil, false # Pre-condition already failed! Do nothing...
-
-              when Symbol # Normal map
-                group, name, type, block =
-                    if  instruction[2].nil? || instruction[2].is_a?(Proc)
-                      [nil] + instruction # No group, [ :name, :type, (:block) ]
-                    else
-                      instruction # [ :group, :name, :type, (:block)]
-                    end
-
-                data = socket.__send__("read_#{type}", &block)
-                if group
-                  @data[group] ||= {}
-                  @data[group][name] = data
-                else
-                  @data[name] = data
-                end
-              else
-                error "Unrecognized instruction #{instruction}"
-            end
-          end
-        end
-      end
-
-      # class AbstractMessage
-
-      ### Actual message classes (short definitions):
+      ### Define short message classes in-line:
 
       AccountValue = def_message([6, 2], [:key, :string],
                                  [:value, :string],
@@ -172,23 +79,25 @@ module IB
                       [:yield, :decimal],
                       [:yield_redemption_date, :int]
 
+      ### Require standalone source files for more complex message classes:
+
+      require 'ib-ruby/messages/incoming/alert'
+      require 'ib-ruby/messages/incoming/contract_data'
+      require 'ib-ruby/messages/incoming/delta_neutral_validation'
+      require 'ib-ruby/messages/incoming/execution_data'
+      require 'ib-ruby/messages/incoming/historical_data'
+      require 'ib-ruby/messages/incoming/market_depths'
+      require 'ib-ruby/messages/incoming/open_order'
+      require 'ib-ruby/messages/incoming/order_status'
+      require 'ib-ruby/messages/incoming/portfolio_value'
+      require 'ib-ruby/messages/incoming/real_time_bar'
+      require 'ib-ruby/messages/incoming/scanner_data'
+      require 'ib-ruby/messages/incoming/ticks'
+
     end # module Incoming
   end # module Messages
 end # module IB
 
-# Require standalone message source files
-require 'ib-ruby/messages/incoming/alert'
-require 'ib-ruby/messages/incoming/contract_data'
-require 'ib-ruby/messages/incoming/delta_neutral_validation'
-require 'ib-ruby/messages/incoming/execution_data'
-require 'ib-ruby/messages/incoming/historical_data'
-require 'ib-ruby/messages/incoming/market_depths'
-require 'ib-ruby/messages/incoming/open_order'
-require 'ib-ruby/messages/incoming/order_status'
-require 'ib-ruby/messages/incoming/portfolio_value'
-require 'ib-ruby/messages/incoming/real_time_bar'
-require 'ib-ruby/messages/incoming/scanner_data'
-require 'ib-ruby/messages/incoming/ticks'
 
 __END__
     // incoming msg id's
---
+++ lib/ib-ruby/messages/outgoing/abstract_message.rb
@@ -0,0 +1,60 @@
+require 'ib-ruby/messages/abstract_message'
+
+module IB
+  module Messages
+    module Outgoing
+
+      # Container for specific message classes, keyed by their message_ids
+      Classes = {}
+
+      class AbstractMessage < IB::Messages::AbstractMessage
+
+        def initialize data={}
+          @data = data
+          @created_at = Time.now
+        end
+
+        # This causes the message to send itself over the server socket in server[:socket].
+        # "server" is the @server instance variable from the IB object.
+        # You can also use this to e.g. get the server version number.
+        #
+        # Subclasses can either override this method for precise control over how
+        # stuff gets sent to the server, or else define a method encode() that returns
+        # an Array of elements that ought to be sent to the server by calling to_s on
+        # each one and postpending a '\0'.
+        #
+        def send_to server
+          self.encode(server).flatten.each do |datum|
+            server[:socket].write_data datum
+          end
+        end
+
+        # At minimum, Outgoing message contains message_id and version.
+        # Most messages also contain (ticker, request or order) :id.
+        # Then, content of @data Hash is encoded per instructions in data_map.
+        def encode server
+          [self.class.message_id,
+           self.class.version,
+           @data[:id] || @data[:ticker_id] || @data[:request_id]|| @data[:order_id] || [],
+           self.class.data_map.map do |(field, default_method, args)|
+             case
+               when default_method.nil?
+                 @data[field]
+
+               when default_method.is_a?(Symbol) # method name with args
+                 @data[field].send default_method, *args
+
+               when default_method.respond_to?(:call) # callable with args
+                 default_method.call @data[field], *args
+
+               else # default
+                 @data[field].nil? ? default_method : @data[field] # may be false still
+             end
+           end
+          ].flatten
+        end
+
+      end # AbstractMessage
+    end # module Incoming
+  end # module Messages
+end # module IB
