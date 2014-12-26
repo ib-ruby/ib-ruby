28475fcd420efa5312ad6bdc9f883a41578bd644
Hartmut Bischoff <beta>
2014-12-26 16:37:43 +0100

    Using FactoryGirl to create Dataobjects. Connect-Defintion are now in connect.yml
    We start with Fatory-Defintitions for Contract, Combo and Bag.
    Target is to include all fragile Dataentries to yml-files. In this case, tests are independent from time and environment.

---
+++ spec/factories/ib_bag.rb
@@ -0,0 +1,62 @@
+FactoryGirl.define do
+	factory :empty_ib_bag, class:IB::Bag do
+		symbol  'GOOG'
+		sec_type  :bag
+		currency 'USD' 
+		exchange 'SMART' 
+		expire '201503'
+	end
+
+	factory :apple_bag, class:IB::Bag do
+		  symbol  'AAPL'
+		 sec_type  :bag
+		   currency  'USD'
+		exchange 'SMART' 
+
+		after(:build) do |bag|  
+			bag.combo_legs << build(:ib_option_contract)
+			bag.combo_legs << build(:ib_option_contract)
+		end
+	end
+	# create a fake butterfly bag
+	factory :butterfly, class:IB::Bag do
+			symbol 'FAKE'
+			exchange 'SMART' 
+			sec_type  :bag
+			currency  'USD'
+			after(:build) do |bag|  
+				[1, -2 , 1].each do | w |
+					bag.combo_legs << build( :combo_leg, weight:w )
+				end
+			end
+
+	end
+
+	factory :butterfliege, class:IB::Bag do
+		## transient definiert Default-Parameter, die im Aufruf überschrieben werden können
+		transient do
+			expire '201503'
+			legs [ 1, 2, 3 ]
+			kind 'PUT'
+			exchange 'SMART' 
+			symbol 'AAPL'
+		end
+		sec_type  :bag
+		currency  'USD'
+		# e --> class::.:: @build_strategy=#<FactoryGirl::Strategy::Build:0x0000000328c208>, @overrides={:expire=>"201501", :legs=>[500, 510, 520], :kind=>"CALL", :symbol=>"AAPL", :sec_type=>:bag, :currency=>"USD", :exchange=>"SMART"}, @cached_attributes={:expire=>"201501", :legs=>[500, 510, 520], :kind=>"CALL", :symbol=>"AAPL", :sec_type=>:bag, :currency=>"USD", :exchange=>"SMART"}, @instance=#<IB::Bag:0x00000003299d90 @attributes={"created_at"=>2014-12-25 15:12:32 +0100, "updated_at"=>2014-12-25 15:12:32 +0100, "con_id"=>0, "right"=>"", "exchange"=>"SMART", "include_expired"=>false, "sec_type"=>"BAG", "symbol"=>"AAPL", "currency"=>"USD"
+		after(:build) do |bag, e|  
+			bag.exchange = e.exchange 
+			bag.symbol = e.symbol
+			list_of_con_ids = e.legs.zip([1,-2,1]).each do | strike, weight |
+				contract = build(:tws_option_contract, symbol:bag.symbol, right:e.kind, strike:strike, expiry:e.expire )
+				bag.combo_legs << build( :combo_leg, weight:weight,  con_id:contract.con_id )
+
+			end
+##			puts "affterm :build"
+#			puts bag.inspect
+#			puts e.expire
+		end
+
+	end
+
+end
---
+++ spec/factories/ib_contract.rb
@@ -0,0 +1,57 @@
+#This will guess the Account class
+FactoryGirl.define do
+	factory :ib_contract, class:IB::Contract do
+		con_id 2586596
+		currency 'USD' 
+		exchange 'SMART' 
+	end
+
+	factory :ib_option_contract, class:IB::Contract do
+		  symbol  'AAPL'
+		 sec_type  :option
+		  expiry  '201503'
+		   strike  110
+		   right  :put
+		   sec_id  'US0378331005'
+		   sec_id_type  'ISIN'
+		   multiplier  10
+		   exchange  'SMART'
+		   currency  'USD'
+		   local_symbol  'AAPL  150320C00110000'
+
+	end
+	# liest einen Optionsdatensatz aus der TWS aus
+
+	factory :tws_option_contract, class:IB::Contract do
+		transient do
+			expire '201503'
+			right :put
+			symbol 'AAPL'
+			strike 110
+		end
+		sec_type  :option
+		multiplier  100
+		currency  'USD'
+		exchange 'SMART' 
+		after(:build) do | con_tract, e |
+			ib = IB::Connection.current.presence ||  IB::Connection.new( OPTS[:connection].merge(:logger => mock_logger))
+			ib.subscribe(:Alert, :ManagedAccounts, :ContractDataEnd) { |msg| puts msg.to_human }
+			ib.wait_for :ManagedAccounts
+			ib.clear_received :ContractData, :ContractDataEnd
+			con_tract.symbol=e.symbol
+			con_tract.strike = e.strike
+			con_tract.right =  e.right
+			con_tract.expiry = e.expiry
+			ib.send_message :RequestContractData, :id => e.strike, :contract => con_tract
+			ib.wait_for :ContractDataEnd, 3
+#			puts "IB::CONTRACT:"
+#			puts ib.received[:ContractData].last.contract.inspect
+			get_contract_data = -> (item){ib.received[:ContractData].last.contract.send(item)}
+			con_tract.con_id= get_contract_data[:con_id]
+#			ib.close_connection
+
+		end
+
+
+	end
+end
