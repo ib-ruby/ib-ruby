0613ada98629e8b24b65986a0c20effabc6d93de
Hartmut Bischoff <beta>
2015-04-01 07:23:36 +0200

    IB::Account#place_order : some improvements

--- lib/models/ib/account.rb
+++ lib/models/ib/account.rb
@@ -82,13 +82,22 @@ class Account < IB::Model
   end
 
 
-  def place_order  order, contract
+  def place_order  order: order, contract: nil
+    order.contract =  contract if order.contract.nil?
     order.account =  account
-    contract.update_contract do | msg |
-      #  con_id and exchange fully qualify a contract, no need to transmit other data
-      tws_contract =  IB::Contract.new con_id: msg.contract.con_id, exchange: msg.contract.exchange
-      IB::Gateway.tws.place_order order, tws_contract
-    end
-  end
+    if order.contract.nil?
+      IB::Gateway.logger.error {"place order --> No Contract specified .::. #{order.to_human}"}
+    else
+      result = order.contract.update_contract do | msg |
+	#  con_id and exchange fully qualify a contract, no need to transmit other data
+	tws_contract =  IB::Contract.new con_id: msg.contract.con_id, exchange: msg.contract.exchange
+	IB::Gateway.tws.place_order order, tws_contract
+      end 
+      unless result.is_a? IB::Contract
+	IB::Gateway.logger.error {"place order --> Invalid Contract specified .::. #{order.to_human}"}
+      end
+
+    end # branch
+  end # place order
 end
 end
