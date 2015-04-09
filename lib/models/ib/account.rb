8c80423fa28bf31f808ef9329426cd74ab5a272b
Hartmut Bischoff <beta>
2015-04-09 06:12:08 +0200

    IB::Account#PlaceOrder: adjust Prices to MinTick

--- lib/models/ib/account.rb
+++ lib/models/ib/account.rb
@@ -104,10 +104,14 @@ The associated Contract overtakes a parallel specified one
 
 The method validates the contract and returns the local_id of the submitted order
 
+Limit- and Aux-Prices are adjusted to Min-Tick, if auto_adjust is specified
 
 =end
 
-  def place_order  order:, contract: nil
+  def place_order  order:, contract: nil, auto_adjust: true
+    # adjust the orderprice to  min-tick
+    adjust = ->(a,b){ a=BigDecimal.new(a,5); b=BigDecimal.new(b,5); o=a.divmod(b); o.last.zero? ? a : a-o.last }
+
     order.contract =  contract if order.contract.nil?
     order.account =  account
     local_id =  nil
@@ -115,8 +119,12 @@ The method validates the contract and returns the local_id of the submitted orde
       IB::Gateway.logger.error {"place order --> No Contract specified .::. #{order.to_human}"}
     else
       result = order.contract.update_contract do | msg |
-	#  con_id and exchange fully qualify a contract, no need to transmit other data
+	if auto_adjust
+	  order.limit_price= adjust.call(order.limit_price, msg.contract_details.min_tick) unless order.limit_price.zero?
+	  order.aux_price= adjust.call(order.aux_price, msg.contract_details.min_tick) unless order.aux_price.zero?
+	end
 	contracts.update_or_create msg.contract, 'con_id'
+	#  con_id and exchange fully qualify a contract, no need to transmit other data
 	tws_contract =  IB::Contract.new con_id: msg.contract.con_id, exchange: msg.contract.exchange
 	local_id = IB::Gateway.current.place_order order, tws_contract
       end 
@@ -134,14 +142,21 @@ Account#ModifyOrder
 
 The order is specified indirectly via local_id or order_ref
 The modification itself is done in the provided block.
+
+Important: The Block has to return the modified IB::Order
 =end
 
-  def modify_order perm_id:nil, local_id: nil, order_ref: nil, &b
+  def modify_order perm_id: nil, local_id: nil, order_ref: nil, &b
+
+    logger.progname 'Gateway#modify_order'
     order = locate_order perm_id: perm_id, local_id: local_id, order_ref: order_ref
     if order.is_a? IB::Order
      order = yield order  # specify modifications in the block
-     IB::Gateway.current.modify_order order, order.contract 
-      
+     if order.is_a? IB::Order
+       IB::Gateway.current.modify_order order, order.contract 
+     else
+       logger.error{ " The Block does not return an IB::Order-Object, instead: #{order.inspect} " }
+     end  
     end
   end
   
