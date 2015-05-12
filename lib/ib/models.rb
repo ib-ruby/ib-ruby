a6e842ae5e02c1c0488278c6be33f6337d15a417
Hartmut Bischoff <beta>
2015-05-12 14:42:23 +0200

    IB::OptionDetails defined, model container used to concentrate and link  Option-Data from TickOption and TickPrice-Requests to an IB::Option-Contract.
    An example, option_data_2, is included, too

--- lib/ib/models.rb
+++ lib/ib/models.rb
@@ -3,6 +3,8 @@
   require 'models/ib/contract_detail'
   require 'models/ib/underlying'
   require 'models/ib/contract'
+  require 'models/ib/option'
+  require 'models/ib/option_detail'
   require 'models/ib/account'
   require 'models/ib/user'
   require 'models/ib/advisor'
