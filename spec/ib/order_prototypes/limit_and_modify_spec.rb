ea1b2d20b1bd337822d44a75c384977ec5227048
Halfgarten Capital <info@mailout.halfgarten-capital.de>
2018-04-26 14:39:27 +0000

    More Tests: Modify Order

--- spec/ib/order_prototypes/limit_and_modify_spec.rb
+++ spec/ib/order_prototypes/limit_and_modify_spec.rb
@@ -1,4 +1,5 @@
 require 'order_helper'
+require 'message_helper'
 
 ## Notice
 ## The first Test-run fails, if active OpenOrder-Messages are recieved 
@@ -18,31 +19,20 @@ RSpec.describe IB::Limit do
 		
 	after(:all) { IB::Connection.current.send_message(:RequestGlobalCancel); close_connection; } 
 
-		let( :the_order_price ){ 56  } #{ get_contract_price.round(1) || 56 }
+		let( :the_order_price ){  ( get_contract_price.round(1)  || 50 ) - 2 }
 		let( :the_submitted_order ) do 
 				 IB::Limit.order price:   the_order_price ,
 												 action: :buy, size: 100, account: ACCOUNT
 		end
 
-	context "Placement section" , focus: true do
-#		before(:all) do
-#			@my_local_id = place_the_order do | last_price |
-#				last_price =  nil
-#				@@the_order_price = last_price.nil? ? 56 : last_price -2    # set a limit price that 
-				#																														 is well below the actual price
-				#
-#				@@the_submitted_order = IB::Limit.order price:   the_order_price.round(1) ,
-#																							 action: :buy, size: 100, account: ACCOUNT
-#			end
-#		end
+	context "Placement section" do
 
 		it "successful placed the order"  do
 			expect( the_submitted_order  ).to be_an IB::Order 
 			expect( the_submitted_order.local_id  ).to be_zero.or be_nil
 			the_local_id =  place_the_order{ the_submitted_order }
-			#expect( IB::Connection.current.received[:OpenOrder].first ).to be_an IB::Messages::Incoming::OpenOrder 
 			expect( the_submitted_order.local_id  ).to be_an( Integer ).and eq( the_local_id ) 
-			puts "SUNMITTED ORDER: #{the_submitted_order.to_human}"
+			puts "SUBMITTED ORDER: #{the_submitted_order.to_human}"
 		end
 		context  IB::Connection  do
 			subject { IB::Connection.current }
@@ -75,6 +65,7 @@ RSpec.describe IB::Limit do
 				expect( o.account ).to  eq ACCOUNT
 			end
 			its( :limit_price ){ is_expected.to eq the_order_price }
+			its( :local_id ){ is_expected.to eq IB::Connection.current.next_local_id - 1 }
 			it 'is basicly the same then the transmitted order' , pending: true do
 				pending "a proper method to compare a received order with on created localy is absent"
 				expect(subject).to be ==the_submitted_order
@@ -82,25 +73,24 @@ RSpec.describe IB::Limit do
 		end  # IB::Order
 	end # placement
 
-	context "Modifing the provided Order", focus: true do
+	context "Modifing the provided Order" do
 		before(:all) do 
 			#IB::Connection.current.received[:OpenOrder].first.order 
 			IB::Connection.current.clear_received :OpenOrder
 		end  # before
 		it "place the modified order " do
-		  @the_modified_order_price = the_order_price - 1
+		  the_modified_order_price = the_order_price - 1
 			the_modified_order = the_submitted_order
 			the_modified_order.local_id = IB::Connection.current.next_local_id - 1
-			the_modified_order.limit_price = @the_modified_order_price
+			the_modified_order.limit_price = the_modified_order_price
 			expect{ place_the_order{ the_modified_order} }.not_to change {  IB::Connection.current.next_local_id }
-			@the_received_open_order = IB::Connection.current.received[:OpenOrder].first 
-			puts "RECEIVED MODIFIED ORDER: #{@the_received_open_order.order.to_human}"
+			the_received_open_order = IB::Connection.current.received[:OpenOrder].first 
+			puts "RECEIVED MODIFIED ORDER: #{the_received_open_order.order.to_human}"
 		end
 #
 		context IB::Messages::Incoming::OpenOrder do
 			subject{ IB::Connection.current.received[:OpenOrder].first }
 #			subject{ @the_received_open_order }
-			it{ puts "RECEIVED ORDER MESSAGE: #{ subject.to_human}" }
 			it_behaves_like 'OpenOrder message'
 
 		end
@@ -116,10 +106,10 @@ RSpec.describe IB::Limit do
 		end
 	end  # modifing I
 
-	context "Modifing the received OpenOrderMessage-Order", focus: true  do
+	context "Modifing the received OpenOrderMessage-Order"  do
 		before(:all) do 
 			@the_received_order = IB::Connection.current.received[:OpenOrder].first.order 
-		#	IB::Connection.current.clear_received :OpenOrder
+			IB::Connection.current.clear_received :OpenOrder
 		end  # before
 
 	context IB::Order do
@@ -135,7 +125,6 @@ RSpec.describe IB::Limit do
 		# initial price
 		expect( @the_received_order.limit_price ).not_to eq the_order_price
 		ib = IB::Connection.current
-			ib.clear_received :OpenOrder
 		modified_order = @the_received_order
 		modified_order.limit_price = the_order_price
 		expect do
@@ -143,10 +132,65 @@ RSpec.describe IB::Limit do
 			ib.wait_for :OpenOrder
 		end.not_to change { ib.next_local_id }    # this proofs that no additional order is created
 
+		expect( ib.received[:OpenOrder]).to have_at_least(1).open_order_message  
 		recieved_order =  ib.received[:OpenOrder].first.order
 		expect( recieved_order).to be == modified_order
 		expect( recieved_order.limit_price ).to eq the_order_price
-		sleep 9	
+		puts "RECEIVED ORDER: #{ recieved_order.to_human}" 
+	end 
+
+	it " modify the amount, submit and receive the modified order" do
+		# initial price
+		IB::Connection.current.clear_received :OpenOrder
+		original_amount =   @the_received_order.total_quantity
+		ib = IB::Connection.current
+		modified_order = @the_received_order
+		modified_order.total_quantity = 123
+		expect do
+			ib.modify_order modified_order, IB::Symbols::Stocks.wfc
+			ib.wait_for :OpenOrder
+		end.not_to change { ib.next_local_id }    # this proofs that no additional order is created
+
+		expect( ib.received[:OpenOrder]).to have_at_least(1).open_order_message  
+		recieved_order =  ib.received[:OpenOrder].first.order
+		expect( recieved_order).to be == modified_order
+		expect( recieved_order.limit_price ).to eq @the_received_order.limit_price
+		expect( recieved_order.total_quantity ).to eq @the_received_order.total_quantity
+		puts "RECEIVED ORDER: #{ recieved_order.to_human}" 
+	end 
+	it " modify  amount and price, submit and receive the modified order" do
+		# initial price
+		IB::Connection.current.clear_received :OpenOrder
+		ib = IB::Connection.current
+		modified_order = @the_received_order
+		modified_order.total_quantity = 23
+		modified_order.limit_price = 5.5
+		expect do
+			ib.modify_order modified_order, IB::Symbols::Stocks.wfc
+			ib.wait_for :OpenOrder
+		end.not_to change { ib.next_local_id }    # this proofs that no additional order is created
+
+		expect( ib.received[:OpenOrder]).to have_at_least(1).open_order_message  
+		recieved_order =  ib.received[:OpenOrder].first.order
+		expect( recieved_order).to be == modified_order
+		expect( recieved_order.limit_price ).to eq @the_received_order.limit_price
+		expect( recieved_order.total_quantity ).to eq @the_received_order.total_quantity
+		puts "RECEIVED ORDER: #{ recieved_order.to_human}" 
+	end 
+	it " modifying  action is not supported"  do
+		# initial price
+		IB::Connection.current.clear_received :OpenOrder, :Alert
+		ib = IB::Connection.current
+		modified_order = @the_received_order
+		modified_order.action = :sell
+		expect do
+			ib.modify_order modified_order, IB::Symbols::Stocks.wfc
+			ib.wait_for :OpenOrder
+		end.not_to change { ib.next_local_id }    # this proofs that no additional order is created
+
+		expect( ib.received[:OpenOrder]).to be_empty  # no OpenOrderRecord created
+		expect( ib.received[:Alert]).to have_at_least(1).alert_message
+		expect( ib.received[:Alert].first.to_human).to  match /TWS Error 105/
 	end 
 
 	end
