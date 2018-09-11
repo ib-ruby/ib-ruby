e8e4d384ee6a363708039f09f041e6573f4960c3
Halfgarten Capital <info@mailout.halfgarten-capital.de>
2018-09-11 13:35:32 +0000

    Gateway#GetAccountInfos: Increasing tolerance timeframe to finish an Request to 2 sec,
    otherwise low-budget hardware occasionally raises IB-Error-Messages

--- lib/ib/account_infos.rb
+++ lib/ib/account_infos.rb
@@ -15,12 +15,15 @@ returns the thread waiting for completing the request
 call it via
 
 	G.get_account_data.join 
+	or
+	G.get_account_data("account-number" ord IB::Account-Object).join
 
 for sequencial processing
 =end
 	def get_account_data *accounts
 		logger.progname = 'Gateway#get_account_data'
-		
+		Delay =  100   #  set the max waiting time (in 1/10-seconds) 
+									 #  in order to set the timeframe before raising an IB::Error	
 			
 		subscribe_id = tws.subscribe( :AccountValue, :PortfolioValue,:AccountDownloadEnd )  do | msg |
 
@@ -64,7 +67,7 @@ for sequencial processing
 				break if account.connected 
 				i +=1
 				sleep 0.1
-				error "Account Infos not correctly processed. Please restart TWS/Gateway." if i > 50
+				error "Account Infos not correctly processed. Please restart TWS/Gateway." if i > Delay
 			end	
 		end
 
