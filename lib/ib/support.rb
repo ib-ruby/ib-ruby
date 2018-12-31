20b8c7671c585e8ef52c16e60a9db9f3709e604a
Halfgarten Capital <info@mailout.halfgarten-capital.de>
2018-12-31 05:48:14 +0000

    forgot to include support.rb and model/ib/condition.rb

---
+++ lib/ib/support.rb
@@ -0,0 +1,152 @@
+
+module IBSupport
+	refine Array do
+
+		def zero?
+			false
+		end
+		# Returns the integer. 
+		# retuns nil otherwise or if no element is left on the stack
+		def read_int
+			i= self.shift  rescue nil
+			i = i.to_i unless i.blank?			# this includes conversion of string to zero(0)
+			i.is_a?( Integer ) ?  i : nil
+		end
+
+		def read_decimal
+			i= self.shift  rescue nil
+			i = i.to_d unless i.blank?
+			i.is_a?(Numeric)  && i < IB::TWS_MAX ?  i : nil  # return nil, if a very large number is transmitted
+		end
+
+		alias read_decimal_max read_decimal
+
+		## Values -1 and below indicate: Not computed (TickOptionComputation)
+		def read_decimal_limit_1
+			i= read_decimal
+			i <= -1 ? nil : i
+		end
+
+		## Values -2 and below indicate: Not computed (TickOptionComputation)
+		def read_decimal_limit_2
+			i= read_decimal
+			i <= -2 ? nil : i
+		end
+
+
+		def read_string
+			self.shift rescue ""
+		end
+		## reads a string and proofs if NULL ==  IB::TWS_MAX is present.
+		## in that case: returns nil. otherwise: returns the string
+		def read_string_not_null
+			r = read_string
+			rd = r.to_d  unless r.blank?
+			rd.is_a?(Numeric) && rd >= IB::TWS_MAX ? nil : r
+		end
+
+		def read_symbol
+			read_string.to_sym
+		end
+
+		# convert xml into a hash
+		def read_xml
+			Ox.load( read_string(), mode: :hash_no_attrs)
+		end
+
+
+		def read_int_date
+			Time.at read_int
+		end
+
+		def read_parse_date
+			Time.parse read_string
+		end
+
+		def read_boolean
+
+			v = self.shift  rescue nil
+			case v
+			when "1"
+				true
+			when "0"
+				false
+			else nil
+			end
+		end
+
+
+		def read_datetime
+			the_string = read_string
+			the_string.blank? ? nil : DateTime.parse(the_string)
+		end
+
+		def read_date
+			the_string = read_string
+			the_string.blank? ? nil : Date.parse(the_string)
+		end
+		#    def read_array
+		#      count = read_int
+		#    end
+
+		## originally provided in socket.rb
+		#    # Returns loaded Array or [] if count was 0#
+		#
+		#    Without providing a Block, the elements are treated as string
+		def read_array hashmode:false,  &block
+			count = read_int
+			case	count 
+			when  0 
+				[]
+			when nil
+				nil
+			else
+				count= count + count if hashmode
+				if block_given?
+					Array.new(count, &block) 
+				else
+					Array.new( count ){ read_string }
+				end
+			end 
+		end
+		#   
+		#  Returns a hash 
+		#  Expected Buffer-Format: 
+		#			count (of Hash-elements)
+		#			count* key|Value
+		#	 Key's are transformed to symbols, values are treated as string
+		def read_hash
+			tags = read_array( hashmode: true )  # { |_| [read_string, read_string] }
+		result =   if 	tags.nil? || tags.flatten.empty?
+								 tags
+							 else
+								 interim = if  tags.size.modulo(2).zero? 
+														 Hash[*tags.flatten]
+													 else 
+														 Hash[*tags[0..-2].flatten]  # omit the last element
+													 end
+								 # symbolize Hash
+								 Hash[interim.map { |k, v| [k.to_sym, v] unless k.nil? }.compact]
+							 end
+		end
+		#
+
+		def read_contract  # read a standard contract and return als hash
+			{	 con_id: read_int,
+				 symbol: read_string,
+				 sec_type: read_string,
+				 expiry: read_string,
+				 strike: read_decimal,
+				 right: read_string,
+				 multiplier: read_int,		
+				 exchange: read_string,
+				 currency: read_string,
+				 local_symbol: read_string,
+				 trading_class: read_string }  # new Version 8
+
+		end
+
+
+		alias read_bool read_boolean
+	end
+end
---
+++ lib/models/ib/condition.rb
@@ -0,0 +1,240 @@
+require 'ib/support'
+module IB
+  class OrderCondition < IB::Model
+    include BaseProperties
+
+
+		prop :operator,									# 1 ->  " >= " , 0 -> " <= "   see /lib/ib/constants # 338f
+				:conjunction_connection,		# "o" -> or  "a"
+				:contract
+
+		def condition_type
+			error "condition_type method is abstract"
+		end
+		def  default_attributes
+			 super.merge(  operator: ">=" , conjunction_connection: :and )
+		end
+
+			
+		
+		def serialize
+			[ condition_type,  self[:conjunction_connection] ]
+		end
+	end
+
+
+
+	class PriceCondition < OrderCondition
+		using IBSupport   # refine Array-method for decoding of IB-Messages
+		prop :price,
+			:trigger_method  # see /models/ib/order.rb# 51 ff	 and /lib/ib/constants # 210 ff
+		
+		def default_attributes 
+			super.merge( :trigger_method => :default  )
+		end
+
+		def condition_type 
+		1
+		end
+
+		def self.make  buffer
+				 m= self.new  conjunction_connection:  buffer.read_string,
+					operator: buffer.read_int,
+					price: buffer.read_decimal
+
+					the_contract = IB::Contract.new con_id: buffer.read_int, exchange: buffer.read_string
+					m.contract = the_contract
+					m.trigger_method = buffer.read_int
+					m
+
+			end
+
+		def serialize
+		super << self[:operator] << price << contract.con_id << contract.primary_exchange  <<  self[:trigger_method] 
+		end
+
+		# dsl:   PriceCondition.fabricate some_contract, ">=", 500
+		def self.fabricate contract, operator, price
+
+			contract.verify! if contract.con_id.to_i.zero? || contract.primary_exchange.empty?
+			self.new	operator: operator,
+								price: price.to_i,
+								contract: contract
+		end
+
+	end
+
+	class TimeCondition < OrderCondition
+		using IBSupport   # refine Array-method for decoding of IB-Messages
+		prop :time
+
+		def condition_type 
+			3
+		end
+
+		def self.make  buffer
+			self.new  conjunction_connection:  buffer.read_string,
+				operator: buffer.read_int,
+				time: buffer.read_parse_date
+		end
+
+		def serialize
+			t =  self[:time]
+			if t.is_a?(String) && t =~ /^\d{8}\z/  # expiry-format yyymmmdd
+				self.time = DateTime.new t[0..3],t[4..5],t[-2..-1]				  
+			end
+			serialized_time = case self[:time]   # explicity formatting of time-object
+												when String
+													self[:time]
+												when DateTime	
+													self[:time].gmtime.strftime("%Y%m%d %H:%M:%S %Z")
+												when  Date, Time
+													self[:time].strftime("%Y%m%d %H:%M:%S")
+												end
+
+			super << self[:operator] << serialized_time 
+		end
+
+		def self.fabricate operator, time
+			self.new operator: operator, 
+							time: time
+		end
+	end
+
+	class ExecutionCondition < OrderCondition
+		using IBSupport   # refine Array-method for decoding of IB-Messages
+		
+		def condition_type 
+			5
+		end
+
+		def self.make  buffer
+			m =self.new  conjunction_connection:  buffer.read_string,
+									 operator: buffer.read_int
+
+			the_contract = IB::Contract.new sec_type: buffer.read_string,
+																			exchange: buffer.read_string,
+																			symbol: buffer.read_string
+			m.contract = the_contract
+			m
+		end
+
+		def serialize
+			super << contract[:sec_type] << contract.primary_exchange << contract.symbol
+		end
+	
+		def self.fabricate c
+			c.verify! if c.con_id.to_i.zero?|| contract.primary_exchange.empty?
+			self.new contract: c
+		end
+
+	end
+
+	class MarginCondition < OrderCondition
+		using IBSupport   # refine Array-method for decoding of IB-Messages
+
+		prop  :percent
+
+		def condition_type 
+			4
+		end
+
+		def self.make  buffer
+			self.new  conjunction_connection:  buffer.read_string,
+								operator: buffer.read_int,
+								percent: buffer.read_int
+
+		end
+
+		def serialize
+		super << self[:operator] << percent 
+		end
+		def self.fabricate operator,  percent
+			self.new operator: operator, 
+							percent: percent
+		end
+	end
+
+
+	class VolumeCondition < OrderCondition
+		using IBSupport   # refine Array-method for decoding of IB-Messages
+
+		prop :volume
+
+		def condition_type 
+		6
+		end
+
+		def self.make  buffer
+			m = self.new  conjunction_connection:  buffer.read_string,
+										operator: buffer.read_int,
+										volumne: buffer.read_int
+
+			the_contract = IB::Contract.new con_id: buffer.read_int, exchange: buffer.read_string
+			m.contract = the_contract
+			m
+		end
+
+		def serialize
+			super << self[:operator] << volume << contract.con_id << contract.primary_exchange 
+		end
+
+		# dsl:   VolumeCondition.fabricate some_contract, ">=", 50000
+		def self.fabricate contract, operator, volume
+			contract.verify! if contract.con_id.to_i.zero? || contract.primary_exchange.empty?
+			self.new	operator: operator,
+								volume: volume,
+								contract: contract
+		end
+	end
+
+	class PercentChangeCondition < OrderCondition
+		using IBSupport   # refine Array-method for decoding of IB-Messages
+		prop :percent_change
+
+		def condition_type 
+		7
+		end
+
+		def self.make  buffer
+				m = self.new  conjunction_connection:  buffer.read_string,
+											operator: buffer.read_int,
+											percent_change: buffer.read_decimal
+
+				the_contract = IB::Contract.new con_id: buffer.read_int, exchange: buffer.read_string
+				m.contract = the_contract
+				m
+		end
+
+		def serialize
+			super << self[:operator] << percent_change  << contract.con_id << contract.primary_exchange
+		end
+		# dsl:   PercentChangeCondition.fabricate some_contract, ">=", "5%"
+		def self.fabricate contract, operator, change
+				contract.verify! if contract.con_id.to_i.zero? || contract.primary_exchange.empty?
+				self.new	operator: operator,
+									percent_change: change.to_i,
+									contract: contract
+		end
+	end
+	class OrderCondition
+		using IBSupport   # refine Array-method for decoding of IB-Messages
+		# subclasses representing specialized condition types.
+
+		Subclasses = Hash.new(OrderCondition)
+		Subclasses[1] = IB::PriceCondition
+		Subclasses[3] = IB::TimeCondition
+		Subclasses[5] = IB::ExecutionCondition
+		Subclasses[4] = IB::MarginCondition
+		Subclasses[6] = IB::VolumeCondition
+		Subclasses[7] = IB::PercentChangeCondition
+
+
+		# This builds an appropriate subclass based on its type
+		#
+		def self.make_from  buffer
+			condition_type = buffer.read_int
+			OrderCondition::Subclasses[condition_type].make( buffer )
+		end
+	end  # class
+end # module
